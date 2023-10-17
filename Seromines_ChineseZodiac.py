af18cf43c389b416b5c6adf2a54f078d88d701de
Ralph Joshua <115396399+everyonehatesralph@users.noreply.github.com>
2023-10-17 09:09:36 -0700

    Add files via upload

---
+++ Seromines_ChineseZodiac.py
@@ -0,0 +1,29 @@
+print("Want to know your Year Animal?")
+year = int(input("Please enter your birth year is: "))
+zodiacyear = year % 12
+
+
+if zodiacyear == 0:
+			 print ("Your animal is: Monkey")
+elif zodiacyear == 1:
+			 print ("Your animal is: Rooster")
+elif zodiacyear == 2:
+			 print ("Your animal is: Dog")
+elif zodiacyear == 3:
+			 print ("Your animal is: Pig")
+elif zodiacyear == 4:
+			 print ("Your animal is: Rat")
+elif zodiacyear == 5:
+			 print ("Your animal is: Ox")
+elif zodiacyear == 6:
+			 print ("Your animal is: Tiger")
+elif zodiacyear == 7:
+			 print ("Your animal is: Rabbit")
+elif zodiacyear == 8:
+			 print ("Your animal is: Dragon")
+elif zodiacyear == 9:
+			 print ("Your animal is: Snake")
+elif zodiacyear == 10:
+			 print ("Your animal is: Horse")
+elif zodiacyear == 11:
+			 print ("Your animal is: Goat")
